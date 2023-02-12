4f45e1e9723eefaf72e992636877831abafff294
Andrew S <samum_94@mail.ru>
2023-02-12 18:22:48 +0300

    Application changed

--- src/PaymentService.java
+++ src/PaymentService.java
@@ -3,18 +3,9 @@ public class PaymentService {
     double interestRate = 9.99;
     double monthlyInterestRate = interestRate / 12 / 100;
 
-    public double calculate1(int oneYear) {
+    public double calculate(int oneYear) {
         double annuityRatio1 = monthlyInterestRate * Math.pow(monthlyInterestRate + 1, oneYear) / (Math.pow(monthlyInterestRate + 1, oneYear) - 1);
         return (annuityRatio1 * loanAmount);
     }
 
-    public double calculate2(int twoYear) {
-        double annuityRatio2 = monthlyInterestRate * Math.pow(monthlyInterestRate + 1, twoYear) / (Math.pow(monthlyInterestRate + 1, twoYear) - 1);
-        return (annuityRatio2 * loanAmount);
-    }
-
-    public double calculate3(int threeYear) {
-        double annuityRatio3 = monthlyInterestRate * Math.pow(monthlyInterestRate + 1, threeYear) / (Math.pow(monthlyInterestRate + 1, threeYear) - 1);
-        return (annuityRatio3 * loanAmount);
-    }
 }
