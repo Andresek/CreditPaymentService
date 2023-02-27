f9a85e95ff0d78a7b1a8cb02c1f4d839dbfb51ea
Andrew S <samum_94@mail.ru>
2023-02-27 15:21:04 +0300

    moved to parameters

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         PaymentService service = new PaymentService();
-        int oneYear = 12;
-        int twoYear = 24;
-        int threeYear = 36;
-        int monthlyPayment1 = (int) service.calculate(oneYear);
-        int monthlyPayment2 = (int) service.calculate(twoYear);
-        int monthlyPayment3 = (int) service.calculate(threeYear);
+        //  int loanAmount = 1_000_000;
+        //  double interestRate = 9.99;
+        int monthlyPayment1 = (int) service.calculate(12, 1_000_000, 9.99);
+        int monthlyPayment2 = (int) service.calculate(24, 1_000_000, 9.99);
+        int monthlyPayment3 = (int) service.calculate(36, 1_000_000, 9.99);
         System.out.println("Кредит на один год: " + monthlyPayment1 + " рублей в месяц");
         System.out.println("Кредит на два года: " + monthlyPayment2 + " рублей в месяц");
         System.out.println("Кредит на три года: " + monthlyPayment3 + " рублей в месяц");
+
     }
 }
--- src/PaymentService.java
+++ src/PaymentService.java
@@ -1,10 +1,7 @@
 public class PaymentService {
-    int loanAmount = 1_000_000;
-    double interestRate = 9.99;
-    double monthlyInterestRate = interestRate / 12 / 100;
-
-    public double calculate(int oneYear) {
-        double annuityRatio1 = monthlyInterestRate * Math.pow(monthlyInterestRate + 1, oneYear) / (Math.pow(monthlyInterestRate + 1, oneYear) - 1);
+    public double calculate(int year, int loanAmount, double interestRate) {
+        double monthlyInterestRate = interestRate / 12 / 100;
+        double annuityRatio1 = monthlyInterestRate * Math.pow(monthlyInterestRate + 1, year) / (Math.pow(monthlyInterestRate + 1, year) - 1);
         return (annuityRatio1 * loanAmount);
     }
 
